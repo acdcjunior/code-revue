e2b7cc42a678d776ce81f8afc9126dd665d1b58b
Antonio Jr <acdcjunior@gmail.com>
2019-01-31 15:01:31 -0200

    feat(app): use vue router

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,26 @@
     <v-app>
         <v-layout justify-center>
             <v-flex xs12>
-                <v-toolbar
-                        color="blue"
-                        tabs
-                        dense
-                >
-
-
-                    <v-tabs
-                            color="blue"
-                            slider-color="yellow"
-                            grow
-                            v-model="selectedtab"
-                    >
+                <v-toolbar color="blue" tabs dense>
+                    <v-tabs color="blue" slider-color="yellow" grow>
                         <v-tabs-slider color="yellow"></v-tabs-slider>
 
-                        <v-tab href="#tab-1">
+                        <v-tab to="/">
                             <v-icon medium>outlined_flag</v-icon> &nbsp;
                             <v-badge color="red">Menções<span slot="badge">2</span></v-badge>
                         </v-tab>
 
-
-                        <v-tab href="#tab-2">
+                        <v-tab to="/commitsarevisar">
                             <v-icon medium>assignment_turned_in</v-icon> &nbsp;
                             <v-badge color="red">Commits a Revisar<span slot="badge">4</span></v-badge>
                         </v-tab>
 
-
-                        <v-tab href="#tab-3">
+                        <v-tab to="/outroscomentarios">
                             <v-icon medium>group</v-icon> &nbsp;
                             <v-badge color="blue darken-1">Outros Comentários<span slot="badge">7</span></v-badge>
                         </v-tab>
 
-
-                        <v-tab href="#tab-4">
+                        <v-tab to="/commits">
                             <v-icon medium>timeline</v-icon> &nbsp;
                             Commits
                         </v-tab>
@@ -53,21 +39,11 @@
                 </v-toolbar>
 
                 <v-content>
-                    <v-tabs-items v-model="selectedtab">
-                        <v-tab-item value="tab-1">
-                            <MencoesView></MencoesView>
-                        </v-tab-item>
-                        <v-tab-item value="tab-2">
-                            <CommitsARevisarView></CommitsARevisarView>
-                        </v-tab-item>
-                        <v-tab-item value="tab-3">
-                            <OutrosComentariosView></OutrosComentariosView>
-                        </v-tab-item>
-                        <v-tab-item value="tab-4">
-                            <CommitsView></CommitsView>
-                        </v-tab-item>
-                    </v-tabs-items>
-                    <center>Logged in as @{{ userLoggedIn }}.</center>
+                    <router-view/>
+                    <v-footer class="pa-3">
+                      <v-spacer></v-spacer>
+                      <div>Logged in as @{{ userLoggedIn }}.</div>
+                    </v-footer>
                 </v-content>
             </v-flex>
         </v-layout>
@@ -75,19 +51,11 @@
 </template>
 
 <script>
-    import MencoesView from "./views/MencoesView";
-    import CommitsARevisarView from "./views/CommitsARevisarView";
-    import OutrosComentariosView from "./views/OutrosComentariosView";
-    import CommitsView from "./views/CommitsView";
-    import {mapGetters} from "vuex";
-    import {GET_USER_LOGGED_IN, USERS} from "./store/modules/users";
 
-    export default {
-        components: {CommitsARevisarView, OutrosComentariosView, CommitsView, MencoesView},
-        data: () => ({
-            selectedtab: 'tab-2'
-        }),
+  import {mapGetters} from "vuex";
+  import {GET_USER_LOGGED_IN, USERS} from "./store/modules/users";
 
+  export default {
         computed: {
             ...mapGetters(USERS, {userLoggedIn: GET_USER_LOGGED_IN}),
         },
---
+++ src/router.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import CommitsARevisarView from "./views/CommitsARevisarView";
+import OutrosComentariosView from "./views/OutrosComentariosView";
+import CommitsView from "./views/CommitsView";
+import MencoesView from "./views/MencoesView";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'mencoes',
+      component: MencoesView
+    },
+    {
+      path: '/commitsarevisar',
+      name: 'CommitsARevisar',
+      component: CommitsARevisarView
+    },
+    {
+      path: '/outroscomentarios',
+      name: 'OutrosComentarios',
+      component: OutrosComentariosView
+    },
+    {
+      path: '/commits',
+      name: 'Commits',
+      component: CommitsView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
+})
+
+
