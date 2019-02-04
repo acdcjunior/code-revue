0a638ef9e454cd230085580f28e8dccbc1807cee
Antonio Jr <acdcjunior@gmail.com>
2019-02-03 23:15:51 -0200

    feat(commit): criar prototipo de CommitsARevisarView

--- src/views/CommitsARevisarView.vue
+++ src/views/CommitsARevisarView.vue
@@ -1,10 +1,37 @@
 <template>
-    <div>Commits a revisar...</div>
+    <v-card>
+        <v-container
+                fluid
+                grid-list-md
+        >
+            <h1>Commits a Revisar</h1>
+            <h4>Total: {{ commits.length }}</h4>
+            <div v-for="(commit, idx) in commits" :key="idx">
+                <CommitResumo :commit="commit" :create_link="true"></CommitResumo>
+            </div>
+        </v-container>
+    </v-card>
 </template>
 
 <script>
+    import CommitResumo from "../components/CommitResumo";
+    import {mapActions, mapGetters} from "vuex";
+    import {COMMITS, FETCH_COMMITS, GET_COMMITS} from "../store/modules/commits";
     export default {
-        name: "CommitsARevisarView"
+        name: "CommitsARevisarView",
+        components: {CommitResumo},
+
+        computed: {
+            ...mapGetters(COMMITS, {commits: GET_COMMITS})
+        },
+
+        mounted() {
+            this.fetchCommits();
+        },
+
+        methods: {
+            ...mapActions(COMMITS, {fetchCommits: FETCH_COMMITS})
+        }
     }
 </script>
 
